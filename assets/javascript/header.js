6ac388e44fee24d12d60e5bdb4e36986ea5d11fa
nfcdjobo <nfcdjobo@gmail.com>
2023-04-22 18:15:45 +0000

    Ajout de connenu du fichier bibliothèques.html et autres

---
+++ assets/javascript/header.js
@@ -0,0 +1,6 @@
+const menu_all = document.getElementById("menu-all");
+menu_all.addEventListener("click", (e)=>{
+    const menu_all_1 = document.getElementById("menu-all-1");
+    menu_all_1.classList.toggle("trouver");
+});
+
